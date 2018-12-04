f5086d2da2d1d24af5c5fff5c80027d11066ce20
jtbamford <tom.bamford5@yahoo.co.uk>
2018-12-04 08:58:41 +0000

    first commit

---
+++ src/main/java/com/qa/BabyApi/BabyApiApplication.java
@@ -0,0 +1,12 @@
+package com.qa.BabyApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyApiApplication.class, args);
+	}
+}
