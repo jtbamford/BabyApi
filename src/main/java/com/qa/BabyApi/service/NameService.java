5dd3f4a4a476cb51b95692c7a64d16bb385ab190
jtbamford <tom.bamford5@yahoo.co.uk>
2018-12-04 14:46:17 +0000

    works

--- src/main/java/com/qa/BabyApi/service/NameService.java
+++ src/main/java/com/qa/BabyApi/service/NameService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.qa.BabyApi.InputException.InputException;
 import com.qa.BabyApi.constants.Constants;
 import com.qa.BabyApi.persistence.domain.Name;
+import com.qa.BabyApi.persistence.domain.POJOName;
 import com.qa.BabyApi.persistence.repository.INameRepository;
 import com.qa.BabyApi.webservices.IConsumeNameGenerator;
 
@@ -18,7 +19,7 @@ public class NameService implements INameService {
 
 	@Autowired
 	private IConsumeNameGenerator namegen;
-	
+
 	@Autowired
 	private JmsTemplate jmsTemplate;
 
@@ -33,39 +34,46 @@ public class NameService implements INameService {
 
 	public Name createName(int length, String word) throws Exception {
 		Name name = new Name();
-		int lengthToGenerate=0;
-		if(word==null) {
-			lengthToGenerate=length;
+		int lengthToGenerate = 0;
+		if (word == null) {
+			lengthToGenerate = length;
 		} else {
-		    lengthToGenerate = length - word.length();
+			lengthToGenerate = length - word.length();
 		}
-		
-		if(word==null && BusinessRules.isWordLengthAllowed(length, word)
-				&& lengthToGenerate >0) {
-			     name.setName(namegen.consumeNameGenerator(lengthToGenerate,word , name)); // implement other API
-			     repo.save(name);
-			     jmsTemplate.convertAndSend("nameQueue",name);
-			     return name;
-		}	
-	
+
+		if (word == null && BusinessRules.isWordLengthAllowed(length, word) && lengthToGenerate > 0) {
+			name.setName(namegen.consumeNameGenerator(lengthToGenerate, word, name)); // implement other API
+			repo.save(name);
+			POJOName nameToAdd = new POJOName();
+			nameToAdd.setId(name.getId());
+			nameToAdd.setName(name.getName());
+			jmsTemplate.convertAndSend("nameQueue", nameToAdd);
+			return name;
+		}
+
 		else if (BusinessRules.isWordAllowed(word) && BusinessRules.isWordLengthAllowed(length, word)
 				&& lengthToGenerate >= 0) {
-			if(lengthToGenerate>0) {
-			name.setName(word + namegen.consumeNameGenerator(lengthToGenerate, word, name)); // implement other API
-			repo.save(name);
-			jmsTemplate.convertAndSend("nameQueue",name);
-			return name;
+			if (lengthToGenerate > 0) {
+				name.setName(word + namegen.consumeNameGenerator(lengthToGenerate, word, name)); // implement other API
+				repo.save(name);
+				POJOName nameToAdd = new POJOName();
+				nameToAdd.setId(name.getId());
+				nameToAdd.setName(name.getName());
+				jmsTemplate.convertAndSend("nameQueue", nameToAdd);
+				return name;
 			} else {
 				name.setName(word);
-			repo.save(name);
-			jmsTemplate.convertAndSend("nameQueue",name);
-			return name;
+				repo.save(name);
+				POJOName nameToAdd = new POJOName();
+				nameToAdd.setId(name.getId());
+				nameToAdd.setName(name.getName());
+				jmsTemplate.convertAndSend("nameQueue", nameToAdd);
+				return name;
 			}
 		} else {
 			throw new InputException();
 		}
 	}
-
 
 	public String updateName(Long id, Name name) {
 		if (repo.findById(id) != null) {
